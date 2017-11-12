3b0b087f12540bb2ae388bc1022e51c2fe4fb959
Patrick O'Shea <p14oshea@gmail.com>
2017-11-12 23:06:37 +0000

    final working. added SpeechManager class

--- app/src/main/java/com/p14osheagmail/okgoogleclone/MainActivity.java
+++ app/src/main/java/com/p14osheagmail/okgoogleclone/MainActivity.java
@@ -1,168 +1,43 @@
 package com.p14osheagmail.okgoogleclone;
 
-import android.Manifest;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.provider.AlarmClock;
-import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.File;
-import java.io.IOException;
-import java.lang.ref.WeakReference;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-import edu.cmu.pocketsphinx.Assets;
-import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
-import edu.cmu.pocketsphinx.Hypothesis;
-import edu.cmu.pocketsphinx.RecognitionListener;
-import edu.cmu.pocketsphinx.SpeechRecognizer;
 
-
-public class MainActivity extends AppCompatActivity implements RecognitionListener{
+public class MainActivity extends AppCompatActivity implements SpeechRecognizerManager.OnResultListener, TextToSpeech.OnInitListener{
 
     TextView s;
-    /* We only need the keyphrase to start recognition, one menu with list of choices,
-       and one word that is required for method switchSearch - it will bring recognizer
-       back to listening for the keyphrase*/
-    private static final String KWS_SEARCH = "wakeup";
-    private static final String MENU_SEARCH = "menu";
-    /* Keyword we are looking for to activate recognition */
-    private static final String KEYPHRASE = "hello";
-
-    /* Used to handle permission request */
-    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
-
-    /* Recognition object */
-    private SpeechRecognizer recognizer;
     private TextToSpeech tts;
+    private SpeechRecognizerManager mSpeechRecognizerManager;
+    private int MY_DATA_CHECK_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         s = (TextView) findViewById(R.id.hello);
-        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
-            return;
-        }
-        // Recognizer initialization is a time-consuming and it involves IO,
-        // so we execute it in async task
-        new SetupTask(this).execute();
+        setmSpeechRecognizerManager();
+
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);
     }
 
-    private static class SetupTask extends AsyncTask<Void, Void, Exception> {
-        WeakReference<MainActivity> activityReference;
-        SetupTask(MainActivity activity) {
-            this.activityReference = new WeakReference<>(activity);
-        }
-        @Override
-        protected Exception doInBackground(Void... params) {
-            try {
-                Assets assets = new Assets(activityReference.get());
-                File assetDir = assets.syncAssets();
-                activityReference.get().setupRecognizer(assetDir);
-            } catch (IOException e) {
-                return e;
-            }
-            return null;
-        }
-        @Override
-        protected void onPostExecute(Exception result) {
-            if (result != null) {
-                ((TextView) activityReference.get().findViewById(R.id.hello)).setText("Failed to init recognizer " + result);
-            } else {
-                activityReference.get().switchSearch(KWS_SEARCH);
-            }
-        }
-    }
-
-    private void setupRecognizer(File assetsDir) throws IOException {
-        recognizer = SpeechRecognizerSetup.defaultSetup().setAcousticModel(new File(assetsDir, "en-us-ptm")).setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
-                // Disable this line if you don't want recognizer to save raw
-                // audio files to app's storage
-                //.setRawLogDir(assetsDir)
-                .getRecognizer();
-        recognizer.addListener(this);
-        // Create keyword-activation search.
-        recognizer.addKeyphraseSearch(KWS_SEARCH, KEYPHRASE);
-        // Create your custom grammar-based search
-        File menuGrammar = new File(assetsDir, "menu.gram");
-        recognizer.addGrammarSearch(MENU_SEARCH, menuGrammar);
-    }
-
-    private void switchSearch(String searchName) {
-        recognizer.stop();
-        if(searchName.equals(KEYPHRASE)){
-            listen();
-        }
-        //recognizer.;
-        if (searchName.equals(KWS_SEARCH))
-            recognizer.startListening(searchName);
-        else
-            recognizer.startListening(searchName, 10000);
-    }
-
-    @Override
-    public void onBeginningOfSpeech() {
-
-    }
-
-    @Override
-    public void onEndOfSpeech() {
-        if (!recognizer.getSearchName().equals(KWS_SEARCH))
-            switchSearch(KWS_SEARCH);
-    }
-
-    @Override
-    public void onPartialResult(Hypothesis hypothesis) {
-
-        if (hypothesis == null){
-            s.setText("Nothing happened");
-            return;
-        }
-        String text = hypothesis.getHypstr();
-        if (text.equals(KEYPHRASE)) {
-            switchSearch(KEYPHRASE);
-        } else if (text.equals("hello")) {
-            s.setText("hello to you");
-        } else if (text.equals("good morning")) {
-            s.setText("Good morning to you too!");
-        } else {
-            s.setText(hypothesis.getHypstr());
-        }
-    }
-
-    @Override
-    public void onResult(Hypothesis hypothesis) {
-        if (hypothesis != null) {
-            s.setText("Result");
-        }
-    }
-
-    @Override
-    public void onError(Exception e) {
-        System.out.println(e.getMessage());
-    }
-
-    @Override
-    public void onTimeout() {
-        switchSearch(KWS_SEARCH);
+    void setmSpeechRecognizerManager(){
+        mSpeechRecognizerManager = new SpeechRecognizerManager(this);
+        mSpeechRecognizerManager.setOnResultListner(this);
     }
 
     private void speak(String text){
@@ -182,43 +57,37 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
         super.onDestroy();
     }
 
-    private void listen(){
-        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something");
-
-        try {
-            startActivityForResult(i, 100);
-        } catch (ActivityNotFoundException a) {
-            Toast.makeText(MainActivity.this, "Your device doesn't support Speech Recognition", Toast.LENGTH_SHORT).show();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                //the user has the necessary data - create the TTS
+                tts = new TextToSpeech(this, this);
+            }
+            else {
+                //no data - install it now
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 100){
-            if (resultCode == RESULT_OK && null != data) {
-                ArrayList<String> res = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                String inSpeech = res.get(0);
-                recognition(inSpeech);
-            }
+    public void onInit(int initStatus) {
+
+        //check for successful instantiation
+        if (initStatus == TextToSpeech.SUCCESS) {
+            if(tts.isLanguageAvailable(Locale.US)==TextToSpeech.LANG_AVAILABLE)
+                tts.setLanguage(Locale.US);
+        }
+        else if (initStatus == TextToSpeech.ERROR) {
+            Toast.makeText(this, "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
         }
     }
 
     private void recognition(String text){
         Log.e("Speech",""+text);
         String[] speech = text.split(" ");
-
-
-        if(text.isEmpty()){
-            speak("didnt not hear that can you repeat ");
-        }
-
-        if(text.contains("what can you do")){
-        }
-
 
         if(text.contains("what") && text.contains("time")){
             SimpleDateFormat digitalTime = new SimpleDateFormat("HH:mm");
@@ -270,5 +139,14 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
         if(text.contains("thank you")){
             speak("Thank you too ");
         }
+    }
+
+    @Override
+    public void OnResult(ArrayList<String> commands) {
+        StringBuilder text = new StringBuilder("");
+        for(String command:commands) {
+            text.append(command).append(" ");
+        }
+        recognition(text.toString());
     }
 }
